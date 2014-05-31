824e25ad2903aa53ba027983acf926a037d07261
Matt Todd <matt.c.todd@gmail.com>
2014-05-31 15:42:20 -0500

    refactored a bit

--- app/db/dbHelper.js
+++ app/db/dbHelper.js
@@ -1,9 +1,18 @@
 var DbHelper = function() {
-   var db = function() {
-      var dbName = 'shoppingList';
+   var username = 'shoppingList'
+      , password = 'shoppingList'
+      , defaultDbName = 'shoppingList'
+      , server = 'localhost'
+      , port = 27017;
 
+   function defaultConnectionString() {
+      return 'mongodb://' + username + ':' + password + '@' + server + ':' + port + '/' + db();
+   }
+
+   function db() {
+      var dbName = defaultDbName;
       if (process.env.NODE_ENV) {
-        dbName = dbName + '-' + process.env.NODE_ENV;
+        dbName += '-' + process.env.NODE_ENV;
       }
       return dbName;
    };
@@ -11,9 +20,9 @@ var DbHelper = function() {
    this.getConnectionString = function() {
       if (process.env.MONGOHQ_URL) {
          return process.env.MONGOHQ_URL;
-      } else {
-         return 'mongodb://localhost:27017/' + db();
       }
+
+      return defaultConnectionString();
    };
 }
 
